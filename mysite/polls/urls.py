034f600ec9705b20fd653faa1919c1a46c94930e
rodann42 <60013635+rodann42@users.noreply.github.com>
2021-02-01 18:02:52 -0700

    update

---
+++ mysite/polls/urls.py
@@ -0,0 +1,16 @@
+from django.urls import path
+from . import views
+
+# this file only get consulted by Django if the URL starts with
+# http://127.0.0.8000/polls/
+from django.urls import path
+
+from . import views
+
+app_name = 'polls'
+urlpatterns = [
+    path('', views.IndexView.as_view(), name='index'),
+    path('<int:pk>/', views.DetailView.as_view(), name='detail'),
+    path('<int:pk>/results/', views.ResultsView.as_view(), name='results'),
+    path('<int:question_id>/vote/', views.vote, name='vote'),
+]
